0727ec0491acca146bf5878051650ca7fc566d4c
DragonKing <patrick.e.king1958@gmail.com>
2026-06-22 09:25:05 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+async function loadAssignments(year, month, day) {
+    const response = await fetch(`/assignments/api/${year}/${month}/${day}/`);
+    return await response.json();
+}
+
+async function updateSong(assignmentId, number, title) {
+    await fetch(`/assignments/api/update-song/${assignmentId}/`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            song_number: number,
+            song_title: title
+        })
+    });
+}
 
